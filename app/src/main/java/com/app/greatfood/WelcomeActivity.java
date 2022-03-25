9f91816f9b1ba8799a5ffa0bcdd0a5b1db639899
Carlos-Aguilar20 <al061736@uacam.mx>
2022-03-25 02:41:37 -0600

    Funcionalidad del boton para que nos diriga a la interfaz principal

---
+++ app/src/main/java/com/app/greatfood/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package com.app.greatfood;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button info = findViewById(R.id.button_welcome);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+}
