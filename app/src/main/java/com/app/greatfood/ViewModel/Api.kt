48fd4fcbcfb90f46ee90ef905e1825ad3196df27
Angelleyva-60876 <71714746+Angelleyva-60876@users.noreply.github.com>
2022-04-01 13:09:28 -0600

    Se creo la estructura y se agrego la Api

---
+++ app/src/main/java/com/app/greatfood/ViewModel/Api.kt
@@ -0,0 +1,7 @@
+package com.app.greatfood.ViewModel
+
+object Api {
+    var Categorias     = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    var Filtar          = "https://www.themealdb.com/api/json/v1/1/filter.php?c={strCategory}"
+    var info    = "https://www.themealdb.com/api/json/v1/1/lookup.php?i={idMeal}"
+}
