d5e1c0525b5b73c4a674b2a7546cb707b444b88a
Carlos-Aguilar20 <al061736@uacam.mx>
2022-04-23 13:35:54 -0500

    Funcionamiento de la ventana bienvenida y conexion a la otra ventana

--- app/src/main/java/com/app/greatfood/WelcomeActivity.java
+++ app/src/main/java/com/app/greatfood/WelcomeActivity.java
@@ -8,19 +8,19 @@ import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        Button info = findViewById(R.id.button_welcome);
-        info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(),MainActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
     }
 
-}
+    public void Ingresar (View view) {
+        Intent ingresar = new Intent (this, MainActivity.class);
+        startActivity(ingresar);
+    }
+
+    }
+
+
