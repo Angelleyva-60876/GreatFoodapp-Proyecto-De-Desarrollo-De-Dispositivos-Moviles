e45dabbb5b16aca34b6d9e1899e3a8d806cf4ca3
Carlos-Aguilar20 <al061736@uacam.mx>
2022-04-23 19:02:30 -0500

    Creando interfaces para mostrar los menus y listas de comida

--- app/src/main/java/com/app/greatfood/activities/WelcomeActivity.java
+++ app/src/main/java/com/app/greatfood/activities/WelcomeActivity.java
@@ -1,10 +1,13 @@
-package com.app.greatfood;
+package com.app.greatfood.activities;
 
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.app.greatfood.R;
+import com.app.greatfood.activities.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
